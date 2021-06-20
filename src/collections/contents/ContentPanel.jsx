4f8ad3fc25db4c107e06c4373d4ad18213807d70
Jonas Rafael Duarte dos Santos <0205917@feevale.br>
2021-06-19 23:10:25 -0300

    home page

--- src/collections/contents/ContentPanel.jsx
+++ src/collections/contents/ContentPanel.jsx
@@ -29,8 +29,18 @@ const ContentPanel = ({ contents }) => {
     // ...contents,
   ];
 
+  const testStyle = {
+    backgroundColor: "#C62B29",
+    color: "#ffffff",
+    fontWeight: "bold",
+  };
+
   return (
     <div className={styles.container}>
+      <Filter
+        list={["race", "class", "item", "feat", "spell", "npc", "monster"]}
+      ></Filter>
+      <Filter list={["bla1", "bla1bla12", "bla1asda"]}></Filter>
       <div className={styles.panel}>
         {_contents.map((Content, i) => (
           <ContentCard key={Content.id + i} content={Content} i={i} />
@@ -41,3 +51,46 @@ const ContentPanel = ({ contents }) => {
 };
 
 export default ContentPanel;
+
+class Filter extends Component {
+  state = { list: [] };
+
+  componentDidMount() {
+    const { list } = this.props;
+    this.setState({
+      list: list.map((name) => ({ name, label: name, active: false })),
+    });
+  }
+
+  toggleActive = (filter) => {
+    filter.active = !filter.active;
+    this.setState({});
+  };
+
+  render() {
+    const { list } = this.state;
+
+    return (
+      <div className={styles.filter}>
+        {list.map((filter) => (
+          <FilterButton
+            key={filter.name}
+            onClick={() => this.toggleActive(filter)}
+            {...filter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+const FilterButton = ({ onClick, active, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={[styles.button, active ? styles.selected : ""].join(" ")}
+    >
+      {label}
+    </div>
+  );
+};
